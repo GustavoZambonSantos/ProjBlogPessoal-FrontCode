1fef90a61517f608cf718c5ba2cca4005e1f3233
GustavoZambonSantos <gudssantos@hotmail.com>
2023-06-13 14:36:07 -0300

    Projeto Blog pessoal

--- PersonalBlog/src/main.tsx
+++ PersonalBlog/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 import Home from './pages/home.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Home />
+    <App />
+
   </React.StrictMode>,
 )
