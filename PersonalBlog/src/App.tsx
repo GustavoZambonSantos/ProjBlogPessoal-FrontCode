34bfb3a9b01c4dc48a3a9e976188c9f808496bc9
GustavoZambonSantos <gudssantos@hotmail.com>
2023-06-16 13:54:49 -0300

    Blog Pessoal

--- PersonalBlog/src/App.tsx
+++ PersonalBlog/src/App.tsx
@@ -1,18 +1,28 @@
 
 import './App.css'
 import { Grid } from '@material-ui/core'
-import Home from './pages/home'
+import Home from './pages/home/home'
 import Navbar from './componentes/navbar/Navbar';
 import Footer from './componentes/footer/Footer';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <>
-      <Navbar />
-       <Home />
-      <Footer />
-    </>
+    <BrowserRouter>
+    <Navbar />
+    <div style={{ minHeight: '60vh' }}>
+    <Routes> // Antigo Switch
+      
+  <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
+    {/*<Route path="/cadastro" element={<CadastroUsuario />} />*/}
+    </Routes>
+    </div>
+    <Footer />
+    </BrowserRouter>
   );
 }
 
--- PersonalBlog/src/main.tsx
+++ PersonalBlog/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Home from './pages/home.tsx'
+import Home from './pages/home/home.tsx'
 import App from './App.tsx'
 
 ReactDOM.render(
